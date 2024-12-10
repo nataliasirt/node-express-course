464cd046fe5e70ffc360d5ccd63950a28c37e92d
nsirtak <nsirtak@gmail.com>
2024-12-09 21:37:28 -0600

    week5-6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+    } catch (error) {
+        console.error(error);
+    }
+};
+start();
